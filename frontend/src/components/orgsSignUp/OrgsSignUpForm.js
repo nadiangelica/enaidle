52e6177a1e25952425f81814b97395d6c49f7c3e
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-02-21 14:26:58 +0000

    Code for Organisation Sign Up Form Component

---
+++ frontend/src/components/orgsSignUp/OrgsSignUpForm.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+
+const OrgsSignUpForm = ({ navigate }) => {
+
+  const [organisationName, setOrganisationName] = useState("");
+  const [email, setEmail] = useState("");
+  const [charityNumber, setCharityNumber] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // '/users' to be amended when we have clarity of route path for backend
+    const response = await fetch('/users', {
+      method: 'POST',
+      body: JSON.stringify({organisationName: organisationName, email: email, charityNumber: charityNumber, password: password}),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    const json = await response.json()
+
+    if (!response.ok) {
+      setError(json.message)
+    }
+
+    if (response.ok) {
+      navigate('/login')
+      setError(null)
+      console.log('Request Submitted')
+    }
+  }
+
+  const handleOrganisationNameChange = (event) => {
+    setOrganisationName(event.target.value)
+  }
+  
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value)
+  }
+
+  const handleCharityNumberChange = (event) => {
+    setCharityNumber(event.target.value)
+  }
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value)
+  }
+
+
+    return (
+      <main>
+        <h2 id='sign-up-title'>Sign up now!</h2>
+        <div className="container">
+          <form className='signUpLoginForm' onSubmit={handleSubmit}>
+            <div className="input-box">
+              <input className='form_field' id="organisation-name" type='text' value={ organisationName } onChange={handleOrganisationNameChange} />
+              <label id='form_label' for='email'>Organisation Name</label >
+              <i></i>
+            </div>
+
+            <div className="input-box">
+              <input className='form_field' id="email" type='text' value={ organisationName } onChange={handleOrganisationNameChange} />
+              <label id='form_label' for='email'>Email</label >
+              <i></i>
+            </div>
+
+            <div className="input-box">
+              <input className='form_field' id="password" type='password' value={ password } onChange={handlePasswordChange} />
+              <label id='form_label' for='password'>Password</label>
+              <i></i>
+            </div> 
+            <input id='submit' type="submit" value="Sign Up" />
+          </form>
+          {error && <div className="error">{error}</div>}
+          </div>
+      </main>
+    );
+}
+
+export default OrgsSignUpForm;
