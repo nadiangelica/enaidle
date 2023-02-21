da2f40ca0eb9d0f6757a85fa9c87881f1a97e917
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-02-21 15:05:33 +0000

    Remove comma  typo on line 13

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <React.StrictMode>
-      <App />,
+      <App />
     </React.StrictMode>
   </BrowserRouter>
 );
