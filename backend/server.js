420f6d3d2a4e02cc3f71777286b9bc0983852aa2
SyntheticDBX <ry.mchugh@outlook.com>
2023-02-21 14:57:00 +0000

    added config for mongodb datbase

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 require('dotenv').config();
-
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
 const express = require('express');
 
 // express app
 const app = express();
+
+// middleware
+app.use(express.json());
 
 app.use((req, res, next) => {
     console.log(req.path, req.method);
@@ -15,6 +19,13 @@ app.get('/', (req, res) => {
     res.json({ message: 'Hello World' });
 });
 // listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('listening for requests on port', process.env.PORT);
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db & listening on port', process.env.PORT);
+        })
+    })
+    .catch((error) => {
+        console.log(error);
 });
