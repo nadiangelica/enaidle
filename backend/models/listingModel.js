a2cddcd12ff9cb71e37334bd75edf979fe0b1a29
nadiaAngelicaKochilas <nadia.kochilas@gmail.com>
2023-02-21 15:26:17 +0000

    Created listing Schema

---
+++ backend/models/listingModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  organisationName:{
+    type: String,
+    required:true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required:true
+  },
+  location: {
+    type: String,
+    required:true
+  },
+  neededByDate: {
+    type: Date,
+    required:true
+  }
+},{ timestamps: true}) 
+
+ module.exports = mongoose.model('Listing', listingSchema)
