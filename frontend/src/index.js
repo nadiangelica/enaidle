51fabce839b389110b7c1556cbe60a64a95f3673
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-02-21 16:52:58 +0000

    Updated code - purpose of creating login page only

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./app/App";
+import App from "../src/components/app/App";
 import { BrowserRouter } from "react-router-dom";
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
