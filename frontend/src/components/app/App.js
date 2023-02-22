3daf0ef6bc407c2c229e60942fee3c7ec10239f0
AndreeaDinculescu <andreeadinculescu90@gmail.com>
2023-02-22 14:31:18 +0000

    HO-16 started code for new request component

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -1,5 +1,6 @@
-import OrgsSignupPage from "../OrgsSignUp/OrgsSignUpForm";
-import OrgsLoginPage from "../OrgsLogin/OrgsLoginForm";
+import OrgsSignupPage from "../orgsSignUp/OrgsSignUpForm";
+import OrgsLoginPage from "../orgsLogin/OrgsLoginForm";
+import NewRequest from "../newRequest/NewRequest";
 import React, { useState } from "react";
 import { useNavigate, Routes, Route } from "react-router-dom";
 
@@ -7,8 +8,18 @@ const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/signup" element={<OrgsSignupPage navigate={useNavigate()} />} />
-        <Route path="/login" element={<OrgsLoginPage navigate={useNavigate()} />} />
+        <Route
+          path="/signup"
+          element={<OrgsSignupPage navigate={useNavigate()} />}
+        />
+        <Route
+          path="/login"
+          element={<OrgsLoginPage navigate={useNavigate()} />}
+        />
+        <Route
+          path="/add-request"
+          element={<NewRequest navigate={useNavigate()} />}
+        />
       </Routes>
     </>
   );
