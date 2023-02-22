74d319c5210c6009b74da1c0e2e68b345552c664
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-02-22 14:49:06 +0000

    added route path for creating a new request

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -1,5 +1,6 @@
 import OrgsSignupPage from "../orgsSignUp/OrgsSignUpForm";
 import OrgsLoginPage from "../orgsLogin/OrgsLoginForm";
+import NewRequest from "../newRequest/NewRequest";
 import React, { useState } from "react";
 import { useNavigate, Routes, Route } from "react-router-dom";
 
@@ -7,18 +8,9 @@ const App = () => {
   return (
     <>
       <Routes>
-        <Route
-          path="/signup"
-          element={<OrgsSignupPage navigate={useNavigate()} />}
-        />
-        <Route
-          path="/login"
-          element={<OrgsLoginPage navigate={useNavigate()} />}
-        />
-        <Route
-          path="/add-request"
-          element={<NewRequest navigate={useNavigate()} />}
-        />
+        <Route path="/signup" element={<OrgsSignupPage navigate={useNavigate()} />} />
+        <Route path="/login" element={<OrgsLoginPage navigate={useNavigate()} />} />
+        <Route path="/new-request" element={<NewRequest navigate={useNavigate()} />} />
       </Routes>
     </>
   );
