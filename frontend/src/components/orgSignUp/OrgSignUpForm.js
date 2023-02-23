2647ed1337702a7e69a27f5ba2604e4b5c51a8b4
AndreeaDinculescu <andreeadinculescu90@gmail.com>
2023-02-23 12:13:20 +0000

    HO-28 updated line 65 to Email Address

--- frontend/src/components/orgSignUp/OrgSignUpForm.js
+++ frontend/src/components/orgSignUp/OrgSignUpForm.js
@@ -56,26 +56,26 @@ const OrgSignUpForm = ({ navigate }) => {
         <div className="container">
           <form className='signUpLoginForm' onSubmit={handleSubmit}>
             <div className="input-box">
-              <input className='form_field' id="organisation-name" type='text' value={ organisationName } onChange={handleOrganisationNameChange} />
               <label id='form_label' htmlFor='email'>Organisation Name</label >
+              <input className='form_field' id="organisation-name" type='text' value={ organisationName } onChange={handleOrganisationNameChange} />
               <i></i>
             </div>
 
             <div className="input-box">
+              <label id='form_label' htmlFor='email'>Email Address</label >
               <input className='form_field' id="email" type='text' value={ email } onChange={handleEmailChange} />
-              <label id='form_label' htmlFor='email'>Email</label >
               <i></i>
             </div>
 
             <div className="input-box">
-              <input className='form_field' id="charity-number" type='text' value={ charityNumber } onChange={handleCharityNumberChange} />
               <label id='form_label' htmlFor='email'>Charity Number (if applicable)</label >
+              <input className='form_field' id="charity-number" type='text' value={ charityNumber } onChange={handleCharityNumberChange} />
               <i></i>
-            </div>
 
+            </div>
             <div className="input-box">
-              <input className='form_field' id="password" type='password' value={ password } onChange={handlePasswordChange} />
               <label id='form_label' htmlFor='password'>Password</label>
+              <input className='form_field' id="password" type='password' value={ password } onChange={handlePasswordChange} />
               <i></i>
             </div> 
             <input id='submit' type="submit" value="Sign Up" />
