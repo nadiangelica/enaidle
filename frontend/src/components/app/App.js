e938b211b0bf7a8fe32815b018a978cc923ef4fd
Sabirah <sabirah.miah.ahammed@gmail.com>
2023-02-25 16:50:28 +0000

    added individual signup form

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -2,6 +2,7 @@ import OrgSignUpForm from "../orgSignUp/OrgSignUpForm";
 import OrgsLoginForm from "../orgLogin/OrgLoginForm";
 import NewRequest from "../newRequest/NewRequest";
 import RequestFeed from "../requestFeed/RequestFeed";
+import IndSignUpForm from "../indSignUp/IndSignUpForm";
 import { useNavigate, Routes, Route } from "react-router-dom";
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
     <>
       <Routes>
         <Route path="/signup" element={<OrgSignUpForm navigate={useNavigate()} />} />
+        <Route path="/individual-signup" element={<IndSignUpForm navigate={useNavigate()} />} />
         <Route path="/login" element={<OrgsLoginForm navigate={useNavigate()} />} />
         <Route path="/new-request" element={<NewRequest navigate={useNavigate()} />} />
         <Route path="/request-feed" element={<RequestFeed navigate={useNavigate()} />} />
---
+++ frontend/src/components/indSignUp/IndSignUpForm.js
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+
+const IndSignUpForm = ({ navigate }) => {
+  const [firstName, setFirstName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const headers = {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "POST, PATCH, OPTIONS",
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const response = await fetch("http://localhost:4000/api/indUsers", {
+      method: "POST",
+      mode: "cors",
+      credentials: "include",
+      headers: headers,
+      body: JSON.stringify({
+        firstName: firstName,
+        surname: surname,
+        email: email,
+        password: password,
+      })
+    });
+
+    const json = await response.json();
+
+    if (!response.ok) {
+      setError(json.message);
+    }
+
+    if (response.ok) {
+      navigate("/login");
+      setError(null);
+      console.log("Request Submitted");
+    }
+  };
+
+  const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value);
+  };
+
+  const handleSurnameChange = (event) => {
+    setSurname(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+    return (
+      <main>
+        <h2 id='sign-up-title'>Sign up now!</h2>
+        <div className="container">
+          <form className='SignUpLoginForm' onSubmit={handleSubmit}>
+            <div className="input-box">
+              <label id='form_label' htmlFor='email'>First Name</label >
+              <input className='form_field' id="first-name" type='text' value={ firstName } onChange={handleFirstNameChange} />
+              <i></i>
+            </div>
+
+            <div className="input-box">
+              <label id='form_label' htmlFor='email'>Last Name</label >
+              <input className='form_field' id="surname" type='text' value={ surname } onChange={handleSurnameChange} />
+              <i></i>
+            </div>
+
+            <div className="input-box">
+              <label id='form_label' htmlFor='email'>Email Address</label >
+              <input className='form_field' id="email" type='text' value={ email } onChange={handleEmailChange} />
+              <i></i>
+            </div>
+
+            <div className="input-box">
+              <label id='form_label' htmlFor='password'>Password</label>
+              <input className='form_field' id="password" type='password' value={ password } onChange={handlePasswordChange} />
+              <i></i>
+            </div> 
+            <input id='submit' type="submit" value="Sign Up" />
+          </form>
+          {error && <div className="error">{error}</div>}
+      </div>
+    </main>
+  );
+};
+
+export default IndSignUpForm;
