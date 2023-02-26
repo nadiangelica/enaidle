4774ecb55376f0f0a727b06b77e1446ca0496a2d
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-02-26 16:21:53 +0000

    HO-43 added routes for Comments in listing

--- backend/routes/listings.js
+++ backend/routes/listings.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const {getAllListings, createListing} = require('../controllers/listingsController');
+const {getAllListings, createListing, getAllComments, addCommentToAd} = require('../controllers/listingsController');
 
 router.get('/' , getAllListings);
 router.post('/' , createListing);
+router.get('/:listing_id/all-comments' , getAllComments);
+router.post('/:listing_id/add-comment' , addCommentToAd);
 
 module.exports = router;
