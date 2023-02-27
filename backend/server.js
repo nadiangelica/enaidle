d8a642d0a3ca138f8e16cef7c9f85fb34377acfa
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-02-27 09:23:55 +0000

    H0-43 added code to run test db or live db

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,6 @@ const listingRoutes = require("./routes/listings");
 const orgUserRoutes = require("./routes/orgUsers");
 const inMemoryDb = require("./spec/mongoMemoryDB");
 
-
 // express app
 const app = express();
 
@@ -15,7 +14,8 @@ app.use(express.json());
 
 // Login middleware
 app.use((req, res, next) => {
-  (req.path, req.method);
+  req.path, req.method;
+  // Comment out console log as it was noise - console.logging all the tests path routes -
   // console.log(req.path, req.method);
   next();
 });
@@ -24,35 +24,33 @@ app.use((req, res, next) => {
 app.use("/api/orgUsers", orgUserRoutes);
 app.use("/api/listings", listingRoutes);
 
-
-
-
-
-if (process.env.NODE_ENV !== 'test') {
-  console.log('-------------------prod', process.env.MONGO_URI)
+if (process.env.NODE_ENV !== "test") {
   mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    app.listen(process.env.PORT, () => {
-      console.log("connected to db & listening on port", process.env.PORT);
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+      app.listen(process.env.PORT, () => {
+        console.log("connected to db & listening on port", process.env.PORT);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
     });
-  })
-  .catch((error) => {
-    console.log(error);
-  });
 } else {
-  // mock mongoe db
-  console.log('-----------------------------test', process.env.MONGO_URI)
-  inMemoryDb.connect()
-  .then(() => {
-    app.listen(process.env.PORT, () => {
-      console.log("connected to test db & listening on port", process.env.PORT);
+  // mock mongo db
+  inMemoryDb
+    .connect()
+    .then(() => {
+      app.listen(process.env.PORT, () => {
+        console.log(
+          "connected to test db & listening on port",
+          process.env.PORT
+        );
+      });
+    })
+    .catch((error) => {
+      console.log(error);
     });
-  })
-  .catch((error) => {
-    console.log(error);
-  });
 }
 
 // listen for requests
-  module.exports = app
+module.exports = app;
