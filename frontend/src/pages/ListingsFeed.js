d817f76fa4402c2d87b67d738b9abc8c9dbebb72
nadiaAngelicaKochilas <nadia.kochilas@gmail.com>
2023-02-28 15:25:53 +0000

    created filtering option(without funtionality

--- frontend/src/pages/ListingsFeed.js
+++ frontend/src/pages/ListingsFeed.js
@@ -4,13 +4,13 @@ import ListingsFeed from '../components/ListingsFeed';
 import "./ListingsFeed.css";
 
 const Listings = () => {
-    const { listings,  dispatch } = useListingsContext();
+    const { listings, dispatch } = useListingsContext();
 
     useEffect(() => {
         const fetchListings = async () => {
             const response = await fetch('/api/listings');
             const json = await response.json();
-            
+
             if (response.ok) {
                 dispatch({ type: 'SET_LISTINGS', payload: json });
             } else {
@@ -21,13 +21,22 @@ const Listings = () => {
     }, [dispatch]);
 
     return (
-        <div className="listings">
-            <h2>LISTINGS</h2>
-            {listings && listings.map((listing) => (
-                <ListingsFeed key={listing._id} listing={listing} />
-            ))}
-        </div>
-    )
+        <div class="dropdown">
+            <div id="myDropdown" class="dropdown-content">
+                <strong>What are you interested in?</strong>
+                <select>
+                    <option value="requirement">Volunteering</option>
+                    <option value="resources">Donation of Goods</option>
+                </select>
+                <div className="listings">
+                    <h2>LISTINGS</h2>
+                    {listings && listings.map((listing) => (
+                        <ListingsFeed key={listing._id} listing={listing} />
+                    ))}
+                </div>
+            </div>
+        </div>)
 }
+
 
 export default Listings;
