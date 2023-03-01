b7fdb1ff1fdf3c45a87a85fbed954d27c045ce66
Ryan McHugh <41973210+SyntheticDBX@users.noreply.github.com>
2023-03-01 13:45:16 +0000

    removed code from listings controller for update a listing.

--- backend/controllers/listingsController.js
+++ backend/controllers/listingsController.js
@@ -1,5 +1,5 @@
-const Listing = require('../models/listingModel');
-const mongoose = require('mongoose');
+const Listing = require("../models/listingModel");
+const mongoose = require("mongoose");
 
 const getAllListings = async (req, res) => {
   const listings = await Listing.find({});
@@ -38,57 +38,16 @@ const createListing = async (req, res) => {
   }
 };
 
-const updateListing = async (req, res) => {
-  const { id } = req.params;
-  const {
-    organisationName,
-    title,
-    requirement,
-    description,
-    address,
-    neededByDate,
-  } = req.body;
-  const requiredFields = {
-    organisationName: "organisationName",
-    title: "title",
-    requirement: "requirement",
-    description: "description",
-    address: "address",
-    neededByDate: "neededByDate",
-  };
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).send(`No listing with id: ${id}`);
-  }
-  try {
-    const listing = await Listing.findByIdAndUpdate(
-      id,
-      {
-        organisationName,
-        title,
-        requirement,
-        description,
-        address,
-        neededByDate,
-      },
-      { new: true }
-    );
-    res.status(200).json(listing);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-};
-
-
 const deleteListing = async (req, res) => {
   const { id } = req.params;
-  
+
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(404).send(`No listing with id: ${id}`);
   }
 
   const listing = await Listing.findByIdAndRemove(id);
 
-  if(!listing) {
+  if (!listing) {
     return res.status(404).send(`No listing with id: ${id}`);
   }
 
