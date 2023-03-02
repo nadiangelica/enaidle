f57eaa0c9b40616683315ea4eeffb3c4eceeba9e
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-03-02 08:11:56 +0000

    added a toggle to display comments

--- frontend/src/components/SingleListingCard.js
+++ frontend/src/components/SingleListingCard.js
@@ -1,15 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import moment from "moment";
 import CommentForm from "../pages/CommentForm";
 
 const SingleListingCard = ({ listing, isLoggedIn }) => {
   const createdDate = moment(listing.createdAt).fromNow();
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [displayedComments, setDisplayedComments] = useState([]);
 
-  const comments = listing.comments.map((comment) => {
+  useEffect(() => {
+    setDisplayedComments(listing.comments.slice(0, 5));
+  }, [listing.comments]);
+
+  const toggleExpanded = () => {
+    setIsExpanded(!isExpanded);
+    if (!isExpanded) {
+      setDisplayedComments(listing.comments);
+    } else {
+      setDisplayedComments(listing.comments.slice(0, 5));
+    }
+  };
+
+  const comments = displayedComments.map((comment, i) => {
     const commentCreatedDate = moment(comment.createdAt).fromNow();
     return (
       <>
-        <p>
+        <p key={i}>
           {comment.userName}: {comment.content}
         </p>
         <p>{commentCreatedDate}</p>
@@ -29,8 +44,11 @@ const SingleListingCard = ({ listing, isLoggedIn }) => {
 
       <hr />
 
-      {isLoggedIn && <CommentForm />}
-      {comments}
+      <button onClick={toggleExpanded}>
+        {isExpanded ? "Hide Comments" : "Comments"}
+      </button>
+      {isExpanded && isLoggedIn && <CommentForm />}
+      {isExpanded && comments}
     </article>
   );
 };
