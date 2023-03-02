d9591447849c0eea4c0f4a821152811782e3b516
Sabirah <sabirah.miah.ahammed@gmail.com>
2023-03-02 19:48:04 +0000

    playing with styling. Please do not look at it right now

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,8 +4,8 @@ import "./index.css";
 import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
 import { ListingsContextProvider } from "./context/ListingsContext";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './custom.scss';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './custom.scss';
 
 
 // import reportWebVitals from './reportWebVitals';
