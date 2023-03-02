739b1bc283339a50ddd748f8df4634974177e445
Nadia-Angelica Kochilas <nadia.kochilas@gmail.com>
2023-03-02 22:03:35 +0000

    it is 10pm we can fix this with bootstrap

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,43 +2,21 @@ import { Link } from 'react-router-dom';
 import { useLogout } from '../hooks/useLogout';
 import { useAuthContext } from '../hooks/useAuthContext';
 import './Navbar.css';
-
 const Navbar = () => {
-
     const { logout } = useLogout();
     const { user } = useAuthContext();
-
     const handleClick = () => {
         logout();
     }
-
     return (
         <header>
-            {/* <nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="/listings">enaidle</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="/listings">Home</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="/listings">Link</a>
-                    </li>
-                </ul>
-                </div>
-            </div>
-            </nav> */}
             <div className="container">
                     <h1>enaidle</h1>
-                <p>
+                <nav>
                     {user && (
                     <div>
                         <Link to="/listings">Listings</Link>
-                        <Link to="/profile">Profile</Link>
+                        {user.type === "org" && <Link to="/profile">Profile</Link>}
                         <Link to="/organisations">Organisations List</Link>
                         <button onClick={handleClick}>Log Out</button>
                     </div>
@@ -52,10 +30,9 @@ const Navbar = () => {
                         <Link to="/organisations">Organisations List</Link>
                     </div>
                     )}
-                </p>
+                </nav>
             </div>
         </header>
     )
 }
-
 export default Navbar;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,8 +4,10 @@ const Home = () => {
             <p className="intro">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
             <h3>Sign up now!</h3>
             <p>I am an:</p>
-            <button className="organisation-button">Organisation</button>
-            <button className="individual-button">Individual enaidler</button>
+            <button className="user-type-redirect-button">Organisation</button>
+            <button className="user-type-redirect-button">Individual</button>
+            <p className="small-paragraph">Already signed up?</p>
+            <button className="login-redirect-button">Login</button>
         </div>
     )
 }
