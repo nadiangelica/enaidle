090be7f408e686aae1e02c57bed75a23f54115c1
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-03-02 08:11:10 +0000

    HO-57 add code to show number of comments on a listing

--- frontend/src/components/ListingsFeed.js
+++ frontend/src/components/ListingsFeed.js
@@ -1,20 +1,24 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 // import { useListingsContext } from '../hooks/useListingsContext';
 
 const ListingsFeed = ({ listing }) => {
-
-    return (
-        <article className="listing" data-cy="listing" key={ listing._id }>
-            <hr />
-            <h4>{listing.title}</h4>
-            <p>listinged by: {listing.organisationName}</p>
-            <p>What's needed: {listing.requirement}</p>
-            <p>Needed by: {listing.neededByDate.slice(0, 10)}</p>
-            <p>Description: {listing.description}</p>
-            <p>Area: {listing.address.city}</p>
-            <Link to={`/listings/${listing._id}`}>View Listing</Link>
-        </article>
-    );
+  return (
+    <article className="listing" data-cy="listing" key={listing._id}>
+      <hr />
+      <h4>{listing.title}</h4>
+      <p>listinged by: {listing.organisationName}</p>
+      <p>What's needed: {listing.requirement}</p>
+      <p>Needed by: {listing.neededByDate.slice(0, 10)}</p>
+      <p>Description: {listing.description}</p>
+      <p>Area: {listing.address.city}</p>
+      {listing.comments.length > 0 && (
+        <div>
+          <span>{listing.comments.length} Comments</span>
+        </div>
+      )}
+      <Link to={`/listings/${listing._id}`}>View Listing</Link>
+    </article>
+  );
 };
 
 export default ListingsFeed;
