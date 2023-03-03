aba91618eb714aa55e456f0d1a96792bdc7c4b4d
mantaslv <mantasvolkas@gmail.com>
2023-03-03 13:08:18 +0000

    Added enaidle logo to navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,6 +2,9 @@ import { Link } from 'react-router-dom';
 import { useLogout } from '../hooks/useLogout';
 import { useAuthContext } from '../hooks/useAuthContext';
 import './Navbar.css';
+import logo from '../assets/images/enaidle_logo.png'
+
+
 const Navbar = () => {
     const { logout } = useLogout();
     const { user } = useAuthContext();
@@ -11,25 +14,25 @@ const Navbar = () => {
     return (
         <header>
             <div className="container">
-                    <h1>en<span style={{ color: '#e32945' }}>aid</span>le</h1>
+                <img src={logo} alt="enaidle" height="200"/>
+                {/* <h1>en<span style={{ color: '#e32945' }}>aid</span>le</h1> */}
                 <nav>
                     {user && (
-                    <div>
-
-                        <Link to="/listings">Opportunities</Link>
-                        {user.type === "org" && <Link to="/profile">Profile</Link>}
-                        <Link to="/organisations">Charity Directory</Link>
-                        <button onClick={handleClick}>Log Out</button>
-                    </div>
+                        <div>
+                            <Link to="/listings">Opportunities</Link>
+                            {user.type === "org" && <Link to="/profile">Profile</Link>}
+                            <Link to="/organisations">Charity Directory</Link>
+                            <button onClick={handleClick}>Log Out</button>
+                        </div>
                     )}
                     { !user && (
-                    <div>
-                        <Link to="/login">Login</Link>
-                        <Link to="/organisation-signup">Organisation Signup</Link>
-                        <Link to="/individual-signup">Individual Signup</Link>
-                        <Link to="/listings">Opportunities</Link>
-                        <Link to="/organisations">Charity Directory</Link>
-                    </div>
+                        <div>
+                            <Link to="/login">Login</Link>
+                            <Link to="/organisation-signup">Organisation Signup</Link>
+                            <Link to="/individual-signup">Individual Signup</Link>
+                            <Link to="/listings">Opportunities</Link>
+                            <Link to="/organisations">Charity Directory</Link>
+                        </div>
                     )}
                 </nav>
             </div>
