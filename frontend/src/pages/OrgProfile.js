c7fc18694456a264905bf79150fd18c27a96f064
Sabirah <sabirah.miah.ahammed@gmail.com>
2023-05-05 16:18:34 +0100

    removed form format for partner profile page
    Co-authored-by: Alicia Moses <AliciaMoses@users.noreply.github.com>

--- frontend/src/pages/OrgProfile.js
+++ frontend/src/pages/OrgProfile.js
@@ -25,22 +25,22 @@ const OrgProfile = () => {
   }, [org, newestInfo]);
 
   // return (
-      // <div className="profileContainer">
-          {/* {newestInfo !== "" && <img src={newestInfo.logoUrl} alt="organisation logo" height="200"/>} */}
+  //     <div className="profileContainer">
+  //         {/* {newestInfo !== "" && <img src={newestInfo.logoUrl} alt="organisation logo" height="200"/>} */}         )}
+  //      </div>
+  //         {/* <p>Organisation Name: {org.organisationName}</p> */}
+  //         {/* {org.charityNumber && <p>Charity Number: {org.charityNumber}</p>} */}
+  //         {/* <p>Email Address: {org.email}</p> */}
 
-          {/* <p>Organisation Name: {org.organisationName}</p> */}
-          {/* {org.charityNumber && <p>Charity Number: {org.charityNumber}</p>} */}
-          {/* <p>Email Address: {org.email}</p> */}
-
-  //         {newestInfo !== "" && (
+  //      {newestInfo !== "" && (
   //             <div className="orgInfo">
-  //                 <p>{newestInfo.missionStatement}</p>
-  //                 <p><a href={newestInfo.websiteUrl}>{newestInfo.websiteUrl}</a></p>
-  //                 <p>Address: {newestInfo.address}</p>
-  //             </div>
-  //         )}
-  //     </div>
+  //                  <p>{newestInfo.missionStatement}</p>
+  //                  <p><a href={newestInfo.websiteUrl}>{newestInfo.websiteUrl}</a></p>
+  //                  <p>Address: {newestInfo.address}</p>
+  //              </div>
+  
   // )
+
   return (
     <div className="account-container">
       <Card>
@@ -56,61 +56,14 @@ const OrgProfile = () => {
             )}
           </Card.Text>
           {/* <Card.Title>{org.organisationName}</Card.Title> */}
-          <Form className="add-new-listing">
-            <Row className="mb-4">
-              <Form.Group as={Col} md="8">
-                <Form.Label>Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  value={org.organisationName}
-                  disabled
-                  readOnly
-                />
-              </Form.Group>
-              <Form.Group as={Col} md="4">
-                <Form.Label>Charity Number</Form.Label>
-                <Form.Control
-                  type="text"
-                  value={org.charityNumber}
-                  disabled
-                  readOnly
-                />
-              </Form.Group>
-            </Row>
-
-            <Row className="mb-4">
-              <Form.Group as={Col} md="6">
-                <Form.Label>Email Address</Form.Label>
-                <Form.Control type="text" value={org.email} disabled readOnly />
-              </Form.Group>
-
-              <Form.Group as={Col} md="6">
-                <Form.Label>Website</Form.Label>
-                <Form.Control
-                   value={org.websiteUrl}
-                  type="text"
-                  placeholder="Website"
-                  disabled
-                  readOnly
-                />
-              </Form.Group>
-            </Row>
-
-            <Row className="mb-4">
-              <Form.Group as={Col} md="12">
-                <Form.Label>Mission Statement</Form.Label>
-                <Form.Control
-                  as="textarea" rows={10} 
-                  type="text"
-                  value={newestInfo.missionStatement}
-                  placeholder="Mission statement"
-                  disabled
-                  readOnly
-                />
-              </Form.Group>
-              <Form.Group as={Col} md="4"></Form.Group>
-            </Row>
-          </Form>
+          <div>
+            {org.organisationName}<br />
+            {org.email}<br />
+            {org.websiteUrl}<br />
+            Charity number: {org.charityNumber}<br />
+            {newestInfo.missionStatement}<br />
+          </div>
+          
         </Card.Body>
       </Card>
     </div>
